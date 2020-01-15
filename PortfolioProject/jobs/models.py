bcd673a8d6c1a48ea73ed01bba4efa49de67ccc9
Adamm Norgren <norgren.adamm@gmail.com>
2020-01-14 18:58:22 -0800

    added postgres db and job model

--- PortfolioProject/jobs/models.py
+++ PortfolioProject/jobs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    """Creates the model for the jobs app."""
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=255)
