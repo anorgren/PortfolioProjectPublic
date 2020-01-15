1afb40baf903326821e7c3dc3852e7a1ee674ea5
Adamm Norgren <norgren.adamm@gmail.com>
2020-01-14 20:22:54 -0800

    Added bootstrap and jobs view in browser with images

--- PortfolioProject/blog/models.py
+++ PortfolioProject/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    """Creates a Blog model"""
+    title = models.CharField(max_length=255)
+    publication_date = models.DateField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
--- PortfolioProject/jobs/views.py
+++ PortfolioProject/jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Job
+
+def home_page(request):
+    """The view for the home page of the app"""
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
